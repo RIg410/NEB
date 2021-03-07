7ce540cc6ae1dfc3dca936345eeb728edb075c2e
Dm. Yakushev <rig410@yandex.ru>
2021-03-07 12:46:24 +0300

    Improve expression parser.

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,16 +1,33 @@
+use std::fmt::{Display, Formatter};
+use std::fmt;
+use std::ops::Range;
+
 use anyhow::{anyhow, Error};
 
 use crate::parser::lexer::{Lexer, Token};
 
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub enum Exp {
-    Value(bool, Val),
-    BinaryOp(Op, Box<Exp>, Box<Exp>),
-    CastToInt(Box<Exp>),
-    CastToFloat(Box<Exp>),
+    Val(Val),
+    Exp {
+        op: Op,
+        left: Box<Exp>,
+        right: Box<Exp>,
+    },
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Clone)]
+impl Display for Exp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Exp::Val(val) => val.fmt(f),
+            Exp::Exp { op, left, right } => {
+                write!(f, "({} {} {})", left, op, right)
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
 pub enum Op {
     // +
     Add,
@@ -26,30 +43,124 @@ pub enum Op {
     Row,
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Clone)]
+impl Display for Op {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            Op::Add => '+',
+            Op::Sub => '-',
+            Op::Mul => '*',
+            Op::Mod => '%',
+            Op::Div => '/',
+            Op::Row => '^',
+        })
+    }
+}
+
+impl Op {
+    pub fn order(&self) -> u8 {
+        match self {
+            Op::Add => 3,
+            Op::Sub => 3,
+            Op::Mul => 2,
+            Op::Mod => 2,
+            Op::Div => 2,
+            Op::Row => 1,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
 pub enum Val {
     Int(i128),
     Float(f64),
 }
 
+impl Display for Val {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Val::Int(val) => val.fmt(f),
+            Val::Float(val) => val.fmt(f),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub enum Sequence {
-    Seq(Vec<Sequence>),
-    Sign(Op),
+    Exp(Exp),
+    Op(Op),
     Operand(Val),
 }
 
-pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
-    let first = lexer.token();
-
-    if first == Token::LParen {
-        lexer.advance()?;
+impl Display for Sequence {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Sequence::Exp(exp) => exp.fmt(f),
+            Sequence::Op(op) => op.fmt(f),
+            Sequence::Operand(op) => op.fmt(f),
+        }
     }
-    let mut last: Option<Token> = None;
+}
 
+impl Sequence {
+    pub fn op(&self) -> Option<Op> {
+        match self {
+            Sequence::Op(op) => Some(*op),
+            _ => None,
+        }
+    }
+
+    pub fn val(&self) -> Option<Val> {
+        match self {
+            Sequence::Operand(val) => Some(*val),
+            _ => None,
+        }
+    }
+
+    pub fn exp(self) -> Option<Exp> {
+        match self {
+            Sequence::Exp(exp) => Some(exp),
+            Sequence::Op(_) => None,
+            Sequence::Operand(val) => Some(Exp::Val(val)),
+        }
+    }
+}
+
+
+pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
+    let mut last: Option<Token> = None;
     let mut seq = vec![];
     loop {
         match lexer.token() {
+            Token::LParen => {
+                if let Some(last) = &last {
+                    if !last.is_sign() {
+                        return Err(anyhow!("Unexpected '(' token. Position: {}", lexer.loc()));
+                    }
+                }
+
+                lexer.advance()?;
+                seq.push(Some(parse_exp(lexer)?));
+                if lexer.token() != Token::RParen {
+                    return Err(anyhow!(
+                            "Unexpected end of input. Expected ')' token. Position: {}",
+                            lexer.loc()
+                        ));
+                }
+            }
+            Token::EOF => {
+                if let Some(last) = &last {
+                    if !(last.is_number() || *last == Token::EOF || *last == Token::RParen) {
+                        return Err(anyhow!(
+                            "Unexpected end of input. Position: {}",
+                            lexer.loc()
+                        ));
+                    }
+                }
+                break;
+            }
+            Token::RParen => {
+                break;
+            }
             Token::IntNumber | Token::FloatNumber => {
                 if let Some(last) = &last {
                     if !last.is_sign() {
@@ -61,15 +172,7 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                     }
                 }
 
-                seq.push(Sequence::Operand(parse_number(false, lexer)?));
-            }
-            Token::LParen => {
-                if let Some(last) = &last {
-                    if !last.is_sign() {
-                        return Err(anyhow!("Unexpected '(' token. Position: {}", lexer.loc()));
-                    }
-                }
-                seq.push(parse_exp(lexer)?);
+                seq.push(Some(Sequence::Operand(parse_number(false, lexer)?)));
             }
             Token::Plus => {
                 if let Some(last) = &last {
@@ -77,18 +180,19 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                         return Err(anyhow!("Unexpected '+' token. Position: {}", lexer.loc()));
                     }
                 }
-                seq.push(Sequence::Sign(Op::Add));
+                seq.push(Some(Sequence::Op(Op::Add)));
             }
             Token::Minus => {
                 if let Some(last) = &last {
                     if last.is_sign() {
                         lexer.advance()?;
-                        seq.push(Sequence::Operand(parse_number(true, lexer)?));
+                        seq.push(Some(Sequence::Operand(parse_number(true, lexer)?)));
                     } else {
-                        seq.push(Sequence::Sign(Op::Sub));
+                        seq.push(Some(Sequence::Op(Op::Sub)));
                     }
                 } else {
-                    seq.push(Sequence::Sign(Op::Sub));
+                    lexer.advance()?;
+                    seq.push(Some(Sequence::Operand(parse_number(true, lexer)?)));
                 }
             }
             Token::Star => {
@@ -97,7 +201,7 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                         return Err(anyhow!("Unexpected '*' token. Position: {}", lexer.loc()));
                     }
                 }
-                seq.push(Sequence::Sign(Op::Mul));
+                seq.push(Some(Sequence::Op(Op::Mul)));
             }
             Token::Slash => {
                 if let Some(last) = &last {
@@ -105,7 +209,7 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                         return Err(anyhow!("Unexpected '/' token. Position: {}", lexer.loc()));
                     }
                 }
-                seq.push(Sequence::Sign(Op::Div));
+                seq.push(Some(Sequence::Op(Op::Div)));
             }
             Token::Caret => {
                 if let Some(last) = &last {
@@ -113,7 +217,7 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                         return Err(anyhow!("Unexpected '^' token. Position: {}", lexer.loc()));
                     }
                 }
-                seq.push(Sequence::Sign(Op::Row));
+                seq.push(Some(Sequence::Op(Op::Row)));
             }
             Token::Percent => {
                 if let Some(last) = &last {
@@ -121,25 +225,7 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
                         return Err(anyhow!("Unexpected '%' token. Position: {}", lexer.loc()));
                     }
                 }
-                seq.push(Sequence::Sign(Op::Mod));
-            }
-            Token::EOF => {
-                if first == Token::LParen {
-                    return Err(anyhow!(
-                        "Unexpected end of input. Expected ')' token. Position: {}",
-                        lexer.loc()
-                    ));
-                } else {
-                    break;
-                }
-            }
-            Token::RParen => {
-                if first == Token::LParen {
-                    lexer.advance()?;
-                    break;
-                } else {
-                    return Err(anyhow!("Unexpected ')' token. Position: {}", lexer.loc()));
-                }
+                seq.push(Some(Sequence::Op(Op::Mod)));
             }
         }
 
@@ -147,10 +233,18 @@ pub fn parse_exp(lexer: &mut Lexer) -> Result<Sequence, Error> {
         lexer.advance()?;
     }
 
-    Ok(Sequence::Seq(seq))
+    if seq.is_empty() {
+        return Err(anyhow!("Empty expression"));
+    }
+
+    if seq.len() == 1 {
+        Ok(seq.remove(0).ok_or_else(|| anyhow!("Empty expression"))?)
+    } else {
+        Ok(Sequence::Exp(make_exp(seq)?))
+    }
 }
 
-pub fn parse_number(negative: bool, lexer: &mut Lexer) -> Result<Val, Error> {
+fn parse_number(negative: bool, lexer: &mut Lexer) -> Result<Val, Error> {
     match lexer.token() {
         Token::IntNumber => {
             let mut val = lexer.content().parse().map_err(|err| {
@@ -178,15 +272,91 @@ pub fn parse_number(negative: bool, lexer: &mut Lexer) -> Result<Val, Error> {
     }
 }
 
+fn make_exp(mut seq: Vec<Option<Sequence>>) -> Result<Exp, Error> {
+    let mut operator_order = seq.iter()
+        .flatten()
+        .enumerate()
+        .filter_map(|(i, sq)| {
+            if let Some(op) = sq.op() {
+                Some((op.order(), i, op))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    operator_order.sort_by(|(l_order, l_index, _), (r_order, r_index, _)| {
+        (*l_order, *l_index).cmp(&(*r_order, *r_index)
+        )
+    });
+
+    let mut buffer: Vec<(Range<usize>, Exp)> = Vec::new();
+
+    fn find_in_buffer(buffer: &mut Vec<(Range<usize>, Exp)>, index: usize) -> Option<(Range<usize>, Exp)> {
+        if let Some(index) = buffer.iter().enumerate()
+            .find_map(|(i, (range, _))| {
+                if range.start <= index && range.end >= index {
+                    Some(i)
+                } else {
+                    None
+                }
+            }) {
+            Some(buffer.remove(index))
+        } else {
+            None
+        }
+    }
+
+    fn find_exp(buffer: &mut Vec<(Range<usize>, Exp)>, seq: &mut Vec<Option<Sequence>>, index: usize) -> Result<(Range<usize>, Exp), Error> {
+        find_in_buffer(buffer, index)
+            .or_else(||
+                seq[index].take()
+                    .and_then(|sq| sq.exp())
+                    .map(|ex| (((index..index), ex)))
+            ).ok_or_else(|| anyhow!("Invalid expiration"))
+    }
+
+    for (_, index, operator) in operator_order {
+        // index may not be 0.
+        let (l_range, l_exp) = find_exp(&mut buffer, &mut seq, index - 1)?;
+        let (r_range, r_exp) = find_exp(&mut buffer, &mut seq, index + 1)?;
+
+        let exp = Exp::Exp {
+            op: operator,
+            left: Box::new(l_exp),
+            right: Box::new(r_exp),
+        };
+
+        buffer.push(((l_range.start..r_range.end), exp))
+    }
+
+    if buffer.len() != 1 {
+        return Err(anyhow!("Invalid expiration"));
+    }
+
+    Ok(buffer.remove(0).1)
+}
+
 #[cfg(test)]
 mod test {
     use crate::parser::ast::parse_exp;
     use crate::parser::lexer::Lexer;
 
+    fn perform_test(input: &str, ir_foot_print: &str) {
+        let mut lexer = Lexer::new(input);
+        lexer.advance().unwrap();
+        assert_eq!(parse_exp(&mut lexer).unwrap().to_string(), ir_foot_print);
+    }
+
     #[test]
     fn test_exp() {
-        let mut lexer = Lexer::new("1 + -2 / (10,1 % 0.1)");
-        lexer.advance().unwrap();
-        dbg!(parse_exp(&mut lexer).unwrap());
+        perform_test("10 * 1 + -2 / 10,1 % 0.1 ^ 1", "((10 * 1) + ((-2 / 10.1) % (0.1 ^ 1)))");
+        perform_test("13", "13");
+        perform_test("10 - 1", "(10 - 1)");
+        perform_test("-10 +  -1", "(-10 + -1)");
+        perform_test("10 - (1 * 2)", "(10 - (1 * 2))");
+        perform_test("(1 * 2) + 10", "((1 * 2) + 10)");
+        perform_test("10", "10");
+        perform_test("(1 + 2) * 10 * (3 - 10^2) ", "(((1 + 2) * 10) * (3 - (10 ^ 2)))");
     }
 }
